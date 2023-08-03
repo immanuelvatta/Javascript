f18543b8bcba8206b11c12c6726e603503ab8cdd
immanuelvatta <immanuelvatta@gmail.com>
2023-08-03 09:57:53 -0500

    test project

---
+++ react/IntroToReact/ReactApp/test-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+      <h1>Hello World</h1>
+      <p>This is a paragraph</p>
+    </>
+    
+  );
+}
+
+export default App;
