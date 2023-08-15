fb72b89eec60fb5c44c9963a1d37227bad6002e6
immanuelvatta <immanuelvatta@gmail.com>
2023-08-15 00:07:36 -0500

    express + mongoose

---
+++ MongoDB/Mongoose/Express+Mongoose/server/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Create a Schema for Users
+const UserSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            required: [true, "First name is required"],
+            minlength: [6, "First name must be at least 6 characters long"]
+        },
+        last_name: {
+            type: String,
+            required: [true, "Last name is required"],
+            maxlength: [20, "Last name must be at most 20 characters long"]
+        },
+        age: {
+            type: Number,
+            min: [1, "You must be at least 1 or older to register"],
+            max: [150, "You must be at most 149 years old to register"]
+        },
+        email: {type: String, required: [true, "Email is required"] }
+    }, 
+    { timestamps: true })
+// create a constructor function for our model and store in variable 'User'
+module.exports = mongoose.model('User', UserSchema);
+
